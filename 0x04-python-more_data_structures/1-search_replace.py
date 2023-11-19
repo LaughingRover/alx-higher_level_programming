7d6bafaa8c0a51636054bf6f195704e9c6870ce3
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-11-19 15:14:41 +0000

    Fix pycode style errors

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	result = []
-	for val in my_list:
-		if val == search:
-			result.append(replace)
-		else:
-			result.append(val)
-			
-	return result
+    return ([replace if x == search else x for x in my_list])
